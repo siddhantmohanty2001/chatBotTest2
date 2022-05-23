e78893be209ee4041f9841ce04f7966369e7b84e
Siddhant Mohanty <siddhantmohanty2001@gmail.com>
2022-05-23 19:37:29 +0530

    testing...

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -93,7 +93,7 @@ async function analyticsLogs() {
 
 async function leadGenaratedLogs(conversationData) {
 	try{
-		console.log("*********",conversationData);
+		console.log("*********MY GOOGLE ",conversationData);
 		await doc.useServiceAccountAuth({
 			client_email: creds.client_email,
 			private_key: creds.private_key,
