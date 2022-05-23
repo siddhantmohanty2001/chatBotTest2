4feb61d5afdcd6b346f1f866807bb8f59fb1753f
Siddhant Mohanty <siddhantmohanty2001@gmail.com>
2022-05-23 23:17:04 +0530

    testing...

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -11,6 +11,9 @@ let testController = {
 		let conversationData = req.body.conversationData;
 		console.log(req.body);
 		const roles=["Program Management","Project Management","Risk Management","Software Quality Management","Configuration Management"]
+		const keys = [['Job Profile', 'Vacancy'],['Job Profile', 'Vacancy'],['Job Profile', 'Vacancy'],['Job Profile', 'Vacancy']]
+const values = [['Project Management', '3'], ['Risk Management', '2'], ['Program Management', '2'], ['Software Quality Management', '10']]
+const title = [['Project Manager'], ['SDE 2'], ['SDE 3'], ['SDE 1']]
 		try {
             		
 			let responseObject = [];
@@ -27,11 +30,24 @@ let testController = {
                                             }
                                         ],
                                         replaceMentValues: [
+											{
+                                                replaceKey: "$title",
+												position:"title",
+                                                replaceIn: "ticketCard",
+                                                replaceValue: title
+                                            },
                                             {
-                                                replaceKey: "$Role",
-                                                replaceIn: "message",
-                                                replaceValue: roles
-                                            }
+                                                replaceKey: "$key",
+												position:"key",
+                                                replaceIn: "ticketCard",
+                                                replaceValue: keys
+                                            },
+											{
+                                                replaceKey: "$value",
+												position:"value",
+                                                replaceIn: "ticketCard",
+                                                replaceValue: values
+                                            },
                                         ]
                                     }
                                 
